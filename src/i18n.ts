831dad942e29845726dc47f877510078ba819410
Valov <m.valov@trucker.group>
2022-09-22 20:06:20 -0300

    Eng

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,8 +4,8 @@ import VueI18n from 'vue-i18n';
 Vue.use(VueI18n);
 
 export default new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'ru',
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'ru',
+  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
+  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
   messages: {
     'en': require('./locales/en.json'),
     'ru': require('./locales/ru.json'),
