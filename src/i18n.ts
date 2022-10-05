587a77f11f0658b5f6f0e24e0e6bba4b47ac0a04
Valov <m.valov@trucker.group>
2022-10-05 14:27:35 -0300

    Releaase 0

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,10 +2,11 @@ import Vue from 'vue';
 import VueI18n from 'vue-i18n';
 
 Vue.use(VueI18n);
+console.log(process.env);
 
 export default new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+  locale: process.env.DEFAULT_LOCALE || 'en',
+  fallbackLocale: process.env.DEFAULT_LOCALE || 'en',
   messages: {
     'en': require('./locales/en.json'),
     'ru': require('./locales/ru.json'),
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,6 +1,6 @@
 import * as axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000/api/v1';
+const BASE_URL = `${process.env.API_HOST}/api/v1`;
 
 function getInitConfig() {
     const url = `${BASE_URL}/convert`;
